8331d7ad72bfdf15db2eda06b9c1f81cc6e3b029
Adam Ravenscroft <adamravenscroft@gmail.com>
2012-11-21 20:02:25 +0000

    implementing SSA renaming

--- src/srt/tool/SSAVisitor.java
+++ src/srt/tool/SSAVisitor.java
@@ -1,11 +1,14 @@
 package srt.tool;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import srt.ast.AssignStmt;
 import srt.ast.Decl;
 import srt.ast.DeclRef;
+import srt.ast.Node;
 import srt.ast.visitor.impl.DefaultVisitor;
 
 public class SSAVisitor extends DefaultVisitor {
@@ -16,26 +19,37 @@ public class SSAVisitor extends DefaultVisitor {
 	}
 	
 	private String getSSAName(String name) {
-		return null;
+		return name + "$" + index.get(name);
 	}
 	
 	private void incrementSSAIndex(String name) {
-		
+		index.put(name, index.get(name) + 1);
 	}
 	
 	@Override
 	public Object visit(Decl decl) {
-		return super.visit(decl);
+		index.put(decl.getName(), 0);
+		Decl renamed = new Decl(getSSAName(decl.getName()), decl.getType(), decl);
+		return super.visit(renamed);
 	}
 	
 	@Override
 	public Object visit(DeclRef declRef) {
-		return super.visit(declRef);
+		DeclRef renamed = new DeclRef(getSSAName(declRef.getName()), declRef);
+		return super.visit(renamed);
 	}
 	
 	@Override
 	public Object visit(AssignStmt assignment) {
-		return super.visit(assignment);
+		Node newRhs = (Node) visit(assignment.getRhs());
+		incrementSSAIndex(assignment.getLhs().getName());
+		Node newLhs = (Node) visit(assignment.getLhs());
+
+		List<Node> children = new ArrayList<Node>();
+		children.add(newLhs);
+		children.add(newRhs);
+		
+		return assignment.withNewChildren(children);
 	}
 
 }
